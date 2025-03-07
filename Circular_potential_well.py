c0b044b3dc737fdb518cefa086454fbe953f1bb1
Danil-Sapojnikov <danil.sapojnikov2@gmail.com>
2025-03-07 17:43:35 +0000

    Add files via upload

--- Circular_potential_well.py
+++ Circular_potential_well.py
@@ -15,11 +15,14 @@ SAVE_FIGURE = False
 FIGURE_NAME = 'Size_accuracy_plot'
 
 L = 1 # length of box
-n = 200 # number of points along each axis of box
-delta = L/n # size of discrete element
+N = 100 # number of points along each axis of box
+delta = L/N # size of discrete element
 
-epsilon = 10e-3
-U_0 = 10e3
+#contour_n = 0
+Num_Eig = 10
+
+epsilon = 10e-5
+U_0 = 10e5
 
 def plot_figure(size, ediffSet):
 
@@ -46,37 +49,77 @@ def plot_figure(size, ediffSet):
 
     return
 
+def plot_contour(XX, YY, prob, contour_N):
+
+    fig = plt.figure(figsize = (6,6))
+    axes_main = fig.add_subplot(111)
+
+    axes_main.set_title(f'Wavefuntion n = {contour_N}',
+                        fontsize = 10, fontweight=550)
+    axes_main.set_xlabel('x', fontsize = 10)
+    axes_main.set_ylabel('y', fontsize = 10)
+
+    axes_main.contourf(XX, YY, prob, levels = 50)
+    
+    #Saves figure
+    if SAVE_FIGURE:
+        plt.savefig(FIGURE_NAME, transparent = True)
+    plt.show()
+
+    plt.close()
+
+    return
+
 def circ_potential(diag):
     
     j = np.linspace(0,m-1,m)
-    y = -L/2 + (np.trunc(j/n)) * delta
+    y = -L/2 + (np.trunc(j/N)) * delta
     
-    i = j - n * np.trunc(j/n)
+    i = j - N * np.trunc(j/N)
     x = -L/2 + i * delta
     
     r = np.sqrt(x**2+y**2)
     
-    return U_0 * np.tanh((r - L/2)/epsilon)
+    return U_0 * np.tanh((r - L/2)/epsilon) +U_0
     
 #discretise axis
-m = n**2
+m = N**2
 diag = np.ones(m) #diagonals
- 
+
+#temp potential plot
+
+#l = circ_potential(diag)
+#l_sqr = np.reshape(l, (-1, N))
+
+#Define grid for contours
+axis_x = np.linspace(-L/2, L/2 - delta, N)
+axis_y = axis_x
+XX, YY = np.meshgrid(axis_x, axis_y)
+
+#plot_contour(XX, YY, l_sqr, contour_n)
 
 #x = + i*delta
 #y = + j*delta
 
 diag0 = diag*(-4) - circ_potential(diag) #main diagonal
 diag1 = diag[0:-1] #second diagonal
-diag1_bounding = diag1[n-1::n]
+diag1_bounding = diag1[N-1::N]
 diag1_bounding[:] = 0 #prevents looping of wavefunction around box
-diagk = diag[0:-n] #k'th diagonal
+diagk = diag[0:-N] #k'th diagonal
 
 #Form differential approximation matrix
-M = scipy.sparse.diags([diag0, diag1, diag1, diagk, diagk], [0, 1, -1, n, -n], format='csc')
+M = scipy.sparse.diags([diag0, diag1, diag1, diagk, diagk], [0, 1, -1, N, -N], format='csc')
 
 #Solve for Eigenvalues
 #evals, evecs = scipy.sparse.linalg.eigs(M, k=10, which='SM')
-evals, evecs = scipy.sparse.linalg.eigsh(M, sigma=0, k=100, which='LM')
+evals, evecs = scipy.sparse.linalg.eigsh(M, sigma=0, k=Num_Eig, which='LM')
 
 evals_sorted = -np.flip(np.sort(evals)) * (1/delta)**2 
+
+for contour_n in range(0, Num_Eig):
+
+    evecs_n = evecs[:,-(contour_n+1)]
+    amp = np.reshape(evecs_n, (-1, N))
+    prob = amp**2
+
+    plot_contour(XX, YY, prob, contour_n)
